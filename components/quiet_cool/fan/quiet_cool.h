37189e91d371c742b0ace881021c7a25998fad0f
greyster1 <grahamferguson@gmail.com>
2025-06-06 18:05:40 -0600

    Update quiet_cool.h

--- components/quiet_cool/fan/quiet_cool.h
+++ components/quiet_cool/fan/quiet_cool.h
@@ -1,40 +1,69 @@
 #pragma once
 
-#include "esphome/components/fan/fan.h"
-#include "esphome/core/component.h"
-#include "esphome/components/spi/spi.h"
-#include "quietcool.h"
-#include <memory>
+#include <Arduino.h>
+#include "esphome/core/defines.h"  // ESPHome compile helpers
+
+/*
+  CC1101 Pin Connections:
+  hdr   cc1101
+  1      19  GND
+  2      18  VCC
+  3      6   GDO0
+  4      7   CSn
+  5      1   SCK
+  6      20  MOSI
+  7      2   MISO
+  8      3   GDO2
+*/
+
+#define FREQ_MHZ  433.897
 
 namespace esphome {
-    namespace quiet_cool {
+namespace quiet_cool {
 
-	class QuietCoolFan : public Component, public fan::Fan, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1KHZ> {
-	public:
-	    void dump_config() override;
-	    fan::FanTraits get_traits() override;
-	    void setup() override;  // initialise radio
-	    float get_setup_priority() const override { return setup_priority::BUS; }
-	    void set_pins(uint8_t csn, uint8_t gdo0, uint8_t gdo2) {
-	        this->csn_pin_ = csn;
-	        this->gdo0_pin_ = gdo0;
-	        this->gdo2_pin_ = gdo2;
-	        this->pins_set_ = true;
-	    }
+enum QuietCoolSpeed {
+    QUIETCOOL_SPEED_HIGH,
+    QUIETCOOL_SPEED_MEDIUM,
+    QUIETCOOL_SPEED_LOW,
+    QUIETCOOL_SPEED_OFF,
+    QUIETCOOL_SPEED_LAST
+};
 
-	protected:
-	    void control(const fan::FanCall &call) override;
-	    void write_state_();
-	    void send_power_command_(bool on);
-	private:
-	    std::unique_ptr<QuietCool> qc_;
+enum QuietCoolDuration {
+    QUIETCOOL_DURATION_1H,
+    QUIETCOOL_DURATION_2H,
+    QUIETCOOL_DURATION_4H,
+    QUIETCOOL_DURATION_8H,
+    QUIETCOOL_DURATION_12H,
+    QUIETCOOL_DURATION_ON,
+    QUIETCOOL_DURATION_LAST
+};
 
-	    uint8_t csn_pin_{};
-	    uint8_t gdo0_pin_{};
-	    uint8_t gdo2_pin_{};
-	    float speed_{0.0f};
-	    bool pins_set_{false};
-	};
+class QuietCool {
+  private:
+    static const char* speed_settings[];
+    static constexpr uint8_t TO_BIT(char c) { return (c == '1') ? 1 : 0; }
 
-    }  // namespace quiet_cool
+    uint8_t csn_pin;
+    uint8_t gdo0_pin;
+    uint8_t gdo2_pin;
+    uint8_t sck_pin;
+    uint8_t miso_pin;
+    uint8_t mosi_pin;
+    unsigned long last_transmission_time; // Track last transmission time
+
+    bool initCC1101();
+    uint8_t readChipVersion();
+    void sendRawData(const char* data, uint8_t len);
+    void sendBits(const char* data, uint8_t len);
+    void sendPacket(const char* data, uint8_t len);
+    const char* getCommand(QuietCoolSpeed speed, QuietCoolDuration duration);
+
+  public:
+    QuietCool(uint8_t csn, uint8_t gdo0, uint8_t gdo2, uint8_t sck, uint8_t miso, uint8_t mosi);
+    void begin();
+    void send(QuietCoolSpeed speed, QuietCoolDuration duration);
+};
+
+}  // namespace quiet_cool
 }  // namespace esphome
